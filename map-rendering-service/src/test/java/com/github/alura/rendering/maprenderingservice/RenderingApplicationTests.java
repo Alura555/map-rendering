5c97a3565a072b0d3c5a935517f05533e0523615
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2024-01-17 11:39:04 +0300

    Add map rendering service

---
+++ map-rendering-service/src/test/java/com/github/alura/rendering/maprenderingservice/RenderingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.alura.rendering.maprenderingservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
