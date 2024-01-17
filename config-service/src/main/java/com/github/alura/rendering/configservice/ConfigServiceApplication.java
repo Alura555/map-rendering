3e63c9659e2bdf8a76128ef814a58abd443dbf7f
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2024-01-17 10:02:39 +0300

    Add config server

---
+++ config-service/src/main/java/com/github/alura/rendering/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.alura.rendering.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
