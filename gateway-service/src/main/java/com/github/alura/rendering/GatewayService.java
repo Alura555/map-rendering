f6f111d741d09dcb9d30ab74e248f1331e5c9b8d
Luiza Abdrashitova <abdrashitova.lr@gmail.com>
2024-01-23 23:28:17 +0300

    Fixed Gateway service issues, now operational

--- gateway-service/src/main/java/com/github/alura/rendering/GatewayService.java
+++ gateway-service/src/main/java/com/github/alura/rendering/GatewayService.java
@@ -3,11 +3,9 @@ package com.github.alura.rendering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class GatewayService {
     public static void main(String[] args) {
         SpringApplication.run(GatewayService.class, args);
